8fb16f1b54a21f3166fd54e8c2a82ff60a4f231d
Mark Wunsch <mark@markwunsch.com>
2010-03-31 15:04:42 -0400

    Parser renders text posts

--- parser/parser.php
+++ parser/parser.php
@@ -31,74 +31,104 @@ class Parser {
 		$this->template = array_merge($this->defaults, Spyc::YAMLLoad($data));
 	}
 	
+	public function block_pattern($block_name) {
+		return '/{block:('.$block_name.')}(.*?){\/block:\\1}/s';
+	}
+	
 	public function parse($document) {
 		// Do big GREP on $document based on page type
 		// return $this->narrow_scope($document);
 		// generate metatags.
-		$posts = $this->template['Posts'];
-		foreach ($posts as $post) {
-			$this->render_post($post, $document);
-		}
-		// return $this->seek($document);
-	}
-	
-	public function get_block($block_name, $doc) {
-		$pattern = '/{block:('.$block_name.')}(.*?){\/block:\\1}/s';
-		$matches = preg_match_all($pattern, $doc, $block);
-		if ($matches && $block[2]) {
-			return $block[2];
-		}
-	}
-	
-	public function render_post($post, $html) {
-		$doc = '';
-		$post_block = $this->get_block('Posts', $html);
-		switch($post['Type']) {
-			case 'Text':
-				$doc = $this->render_text_post($post, $post_block, $html);
-				break;
-		}
+		
+		$doc = $this->get_posts($document);
+		
+		// Finally, generate global values
+		
 		return $doc;
 	}
 	
-	public function narrow_scope($scope, $block='') {
-		$does_match = preg_match_all($this->blocks, $scope, $matcher);
-		$doc = '';
-				
-		if ($does_match){
-			foreach ($matcher[2] as $context) {
-				$doc .= $this->narrow_scope($context);
-			}
-			return $doc;
-		} else {
-			return $scope;
-			// return preg_replace($this->variables, $scope ,$scope);
-		}
+	public function get_posts($document) {
+		$html = preg_replace_callback(
+			$this->block_pattern('Posts'),
+			array($this, 'render_posts'),
+			$document
+		);
+		return $html;
 	}
 	
-	public function seek($context) {
-		return preg_replace_callback($this->variables, array($this, 'convert_properties'), $context);
-	}
-	
-	protected function convert_properties($match) {
-		if (array_key_exists($match[1], $this->template)) {
-			return $this->template[$match[1]];
+	public function render_posts($matches) {
+		$block = $matches[2];
+		$html = '';
+		$posts = $this->template['Posts'];
+		foreach ($posts as $post) {
+			$html = $this->render_post($post, $block);
 		}
+		return $html;
 	}
-	
-	protected function render_text_post($post, $block, $document) {
-		foreach ($block as $html) {
-			$text = $this->get_block('Text', $html);
-			foreach ($text as $text_block) {
-				$text_post = preg_replace('/{Body}/', $post['Body'], $text_block);
-				if ($post['Title']) {
-					$text_post = preg_replace('/{Title}/', $post['Title'], $text_post);
-				}
-				// print_r($text_post);
-			} 
-		}
 
+	
+	public function render_post($post, $block) {
+		$html = '';
+		switch($post['Type']) {
+			case 'Text':
+				$html = $this->render_text_post($post, $block);
+				break;
+		}
+		return $html;
 	}
+	
+	protected function render_text_post($post, $block) {
+		$pattern = $this->block_pattern($post['Type']);
+		$does_match = preg_match_all($pattern, $block, $posts);
+		if ($does_match) {
+			$html = '';
+			foreach ($posts[2] as $index => $text) {
+				$text = preg_replace('/{Body}/', $post['Body'], $text);
+				if ($post['Title']) {
+					$text = preg_replace('/{Title}/', $post['Title'], $text);
+					$text = preg_replace_callback(
+						$this->block_pattern('Title'),
+						create_function(
+							'$matches',
+							'return $matches[2];'
+						),
+						$text
+					);
+				} else {
+					$text = preg_replace($this->block_pattern('Title'), '', $text);
+				}
+				
+				$html = preg_replace($pattern, $text, $block, 1);
+			}
+			return $html;
+		}
+	}
+	
+	
+	// public function narrow_scope($scope, $block='') {
+	// 	$does_match = preg_match_all($this->blocks, $scope, $matcher);
+	// 	$doc = '';
+	// 			
+	// 	if ($does_match){
+	// 		foreach ($matcher[2] as $context) {
+	// 			$doc .= $this->narrow_scope($context);
+	// 		}
+	// 		return $doc;
+	// 	} else {
+	// 		return $scope;
+	// 		// return preg_replace($this->variables, $scope ,$scope);
+	// 	}
+	// }
+	// 
+	// public function seek($context) {
+	// 	return preg_replace_callback($this->variables, array($this, 'convert_properties'), $context);
+	// }
+	// 
+	// protected function convert_properties($match) {
+	// 	if (array_key_exists($match[1], $this->template)) {
+	// 		return $this->template[$match[1]];
+	// 	}
+	// }
 	
 }
 
