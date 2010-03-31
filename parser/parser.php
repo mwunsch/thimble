8d971557b6eed791290fd689386209e06b04d0e4
Mark Wunsch <mark@markwunsch.com>
2010-03-31 17:51:20 -0400

    Formatting multiple posts. Like a boss.

--- parser/parser.php
+++ parser/parser.php
@@ -6,7 +6,7 @@ class Parser {
 	
 	protected $variables = '/{([A-Za-z][A-Za-z0-9\-]*)}/';
 	
-	protected $blocks = '/{(block:[A-Za-z][A-Za-z0-9]*)}(.*?){\/\\1}/s';
+	protected $blocks = '/{block:([A-Za-z][A-Za-z0-9]*)}(.*?){\/block:\\1}/s';
 	
 	public $data = array();
 	
@@ -21,7 +21,8 @@ class Parser {
 		'PortraitURL-48' 	=> "http://assets.tumblr.com/images/default_avatar_48.gif",
 		'PortraitURL-64' 	=> "http://assets.tumblr.com/images/default_avatar_64.gif",
 		'PortraitURL-96' 	=> "http://assets.tumblr.com/images/default_avatar_96.gif",
-		'PortraitURL-128' 	=> "http://assets.tumblr.com/images/default_avatar_128.gif"
+		'PortraitURL-128' 	=> "http://assets.tumblr.com/images/default_avatar_128.gif",
+		'CopyrightYears'	=> '2007-2010'
 	);
 	
 	public $template = array();	
@@ -42,12 +43,15 @@ class Parser {
 		
 		$doc = $this->get_posts($document);
 		
-		// Finally, generate global values
+		if ($this->template['Description']) {
+			$doc = $this->render_block('Description', $doc);
+		}
 		
-		return $doc;
+		// Finally, generate global values		
+		return $this->seek($doc);
 	}
 	
-	public function get_posts($document) {
+	public function get_posts($document) {		
 		$html = preg_replace_callback(
 			$this->block_pattern('Posts'),
 			array($this, 'render_posts'),
@@ -60,13 +64,27 @@ class Parser {
 		$block = $matches[2];
 		$html = '';
 		$posts = $this->template['Posts'];
-		foreach ($posts as $post) {
-			$html = $this->render_post($post, $block);
-		}
+		foreach ($posts as $index => $post) {
+			//render non-post blocks
+			$html .= $this->render_post($post, $this->filter_by_post_type($post, $block));
+		}		
 		return $html;
 	}
-
 	
+	public function filter_by_post_type($post, $block) {
+		$post_type = $this->block_pattern($post['Type']);
+		$found = preg_match_all($post_type, $block, $posts);
+		if ($found) {
+			$split = preg_split($post_type, $block);
+			$stripped = array();
+			foreach ($split as $component) {
+				$stripped[] = preg_replace($this->blocks, '', $component);
+			}
+			$html = implode(implode($posts[0]),$stripped);
+			return $html;
+		}
+	}
+		
 	public function render_post($post, $block) {
 		$html = '';
 		switch($post['Type']) {
@@ -79,31 +97,45 @@ class Parser {
 	
 	protected function render_text_post($post, $block) {
 		$pattern = $this->block_pattern($post['Type']);
+
 		$does_match = preg_match_all($pattern, $block, $posts);
 		if ($does_match) {
+			
 			$html = '';
 			foreach ($posts[2] as $index => $text) {
-				$text = preg_replace('/{Body}/', $post['Body'], $text);
+				$html = preg_replace('/{Body}/', $post['Body'], $text);
 				if ($post['Title']) {
-					$text = preg_replace('/{Title}/', $post['Title'], $text);
-					$text = preg_replace_callback(
-						$this->block_pattern('Title'),
-						create_function(
-							'$matches',
-							'return $matches[2];'
-						),
-						$text
-					);
+					$html = preg_replace('/{Title}/', $post['Title'], $html);
+					$html = $this->render_block('Title', $html);
 				} else {
-					$text = preg_replace($this->block_pattern('Title'), '', $text);
+					$html = preg_replace($this->block_pattern('Title'), '', $html);
 				}
-				
-				$html = preg_replace($pattern, $text, $block, 1);
+				$html = preg_replace($pattern, $html, $block, 1);	
 			}
 			return $html;
 		}
 	}
 	
+	public function render_block($name, $html) {
+		return preg_replace_callback(
+			$this->block_pattern($name),
+			create_function(
+				'$matches',
+				'return $matches[2];'
+			),
+			$html
+		);	
+	}
+	
+	public function seek($context) {
+		return preg_replace_callback($this->variables, array($this, 'convert_properties'), $context);
+	}
+	
+	protected function convert_properties($match) {
+		if (array_key_exists($match[1], $this->template)) {
+			return $this->template[$match[1]];
+		}
+	}
 	
 	// public function narrow_scope($scope, $block='') {
 	// 	$does_match = preg_match_all($this->blocks, $scope, $matcher);
@@ -119,16 +151,7 @@ class Parser {
 	// 		// return preg_replace($this->variables, $scope ,$scope);
 	// 	}
 	// }
-	// 
-	// public function seek($context) {
-	// 	return preg_replace_callback($this->variables, array($this, 'convert_properties'), $context);
-	// }
-	// 
-	// protected function convert_properties($match) {
-	// 	if (array_key_exists($match[1], $this->template)) {
-	// 		return $this->template[$match[1]];
-	// 	}
-	// }
+	//
 	
 }
 
