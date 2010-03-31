e637f45d0cf0a4c61c7888346072bf601b09e2c5
Mark Wunsch <mark@markwunsch.com>
2010-03-30 22:37:01 -0400

    Demo data and YAML parsing

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 // load the parser
 require('parser/parser.php');
 
-$theme = new Parser();
+$data = file_get_contents('data/demo.yml');
+$theme = new Parser($data);
 
-$document = file_get_contents('themes/test.html');
+$document = file_get_contents('themes/redux.html');
+
 print_r($theme->parse($document));
+
+
 
 ?>
--- parser/parser.php
+++ parser/parser.php
@@ -1,43 +1,89 @@
 <?php
 
+require_once 'spyc.php';
+
 class Parser {
 	
-	protected $variables = '/{([A-Za-z][A-Za-z0-9]*)}/';
+	protected $variables = '/{([A-Za-z][A-Za-z0-9\-]*)}/';
 	
-	protected $blocks = '';
+	protected $blocks = '/{(block:[A-Za-z][A-Za-z0-9]*)}(.*?){\/\\1}/s';
 	
-	public $template = array();
+	public $data = array();
 	
-	public $defaults = array(
-		'Title' => 'Hello world'
-	);
+	public $type = '';
 	
-	public $properties = array();
+	public $defaults = array();
 	
-	public function __construct($template = array()) {
-		$this->template = array_merge($this->defaults, $template);
+	public $template = array();	
+		
+	public function __construct($data = array(), $type = 'index') {
+		$this->type = $type;
+		$this->template = array_merge($this->defaults, Spyc::YAMLLoad($data));
 	}
 	
 	public function parse($document) {
-		preg_match_all($this->variables, $document, $matches);
-		$this->properties = $this->map($matches[1]);
-		return preg_replace_callback($this->variables, array($this, 'replace'), $document);
+		// Do big GREP on $document based on page type
+		// return $this->narrow_scope($document);
+		// generate metatags.
+		$posts = $this->template['Posts'];
+		foreach ($posts as $post) {
+			$this->render_post($post, $document);
+		}
+		// return $this->seek($document);
 	}
 	
-	public function map($match) {
-		$properties = array();
-		foreach	($match as $property) {
-			if (in_array($property, array_keys($this->template))) {
-				$properties[$property] = $this->template[$property];
+	public function get_block($block_name, $doc) {
+		$pattern = '/{block:('.$block_name.')}(.*?){\/block:\\1}/s';
+		$matches = preg_match_all($pattern, $doc, $block);
+		if ($matches && $block[2]) {
+			return $block[2];
+		}
+	}
+	
+	public function render_post($post, $html) {
+		$doc = '';
+		$post_block = $this->get_block('Posts', $html);
+		switch($post['Type']) {
+			case 'Text':
+				$doc = $this->render_text_post($post, $post_block, $html);
+				break;
+		}
+		return $doc;
+	}
+	
+	public function narrow_scope($scope, $block='') {
+		$does_match = preg_match_all($this->blocks, $scope, $matcher);
+		$doc = '';
+				
+		if ($does_match){
+			foreach ($matcher[2] as $context) {
+				$doc .= $this->narrow_scope($context);
 			}
+			return $doc;
+		} else {
+			return $scope;
+			// return preg_replace($this->variables, $scope ,$scope);
 		}
-		return $properties;		
 	}
 	
-	public function replace($matches) {
-		if (array_key_exists($matches[1], $this->properties)) {
-			return $this->properties[$matches[1]];
+	public function seek($context) {
+		return preg_replace_callback($this->variables, array($this, 'convert_properties'), $context);
+	}
+	
+	protected function convert_properties($match) {
+		if (array_key_exists($match[1], $this->template)) {
+			return $this->template[$match[1]];
 		}
+	}
+	
+	protected function render_text_post($post, $block, $document) {
+		foreach ($block as $html) {
+			$text = $this->get_block('Text', $html);
+			foreach ($text as $text_block) {
+				$post = preg_replace('/{Body}/', $post['Body'], $text_block);				
+			} 
+		}
+
 	}
 	
 }
