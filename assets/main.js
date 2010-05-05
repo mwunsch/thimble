ba058158800339bb911722d3dab99cdb578513da
Mark Wunsch <mark@markwunsch.com>
2010-05-05 16:13:16 -0400

    Thimble Poll

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,26 @@
+var THIMBLE_POLL;
+
+function ThimblePoll(theme, iframe) {
+  $.ajax({
+    type: 'HEAD',
+    url: 'theme.php?theme='+theme,
+    ifModified: true,
+    success: function(data, status, xhr) {
+      if (status !== "notmodified") {
+        iframe.src = iframe.src;
+      }
+      THIMBLE_POLL = setTimeout(function(){
+        ThimblePoll(theme, iframe);        
+      }, 5000);
+    }
+  });
+  return THIMBLE_POLL;
+}
+
 $(document).ready(function(){
   var form = $('#theme-select');
   var select = form.children('#theme-selector');
+  var refresh = form.children('#auto-refresh');
   var iframe = $('#theme-preview');
   var hash = window.location.hash;
   
@@ -12,12 +32,24 @@ $(document).ready(function(){
       location = window.location.href;
     }
     window.location = location+"#/" + select.children(':selected').val();
+    clearTimeout(THIMBLE_POLL);
     iframe.attr('src','theme.php?'+$(this).serialize());
+    if (refresh.is(':checked')) {
+      ThimblePoll(select.children(':selected').val(), iframe.get(0));
+    }
     return false;
   });
   
   select.bind('change',function(e){
     form.trigger('submit');
+  });
+  
+  refresh.bind('change',function(e){
+    if (refresh.is(':checked')) {
+      ThimblePoll(select.children(':selected').val(), iframe.get(0));
+    } else {
+      clearTimeout(THIMBLE_POLL);
+    }
   });
   
   if (hash) {
@@ -26,4 +58,5 @@ $(document).ready(function(){
   } else {
     form.trigger('submit');
   }
+  
 });
