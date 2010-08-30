2e16c1511abb007d6dbb0a1b5a8720315057ed80
Mark Wunsch <mark@markwunsch.com>
2010-08-30 00:26:05 -0400

    Meta Options parsing appearance and JS

--- assets/main.js
+++ assets/main.js
@@ -17,13 +17,74 @@ function ThimblePoll(theme, iframe) {
   return THIMBLE_POLL;
 }
 
+
 $(document).ready(function(){
   var form = $('#theme-select');
   var select = form.children('#theme-selector');
   var refresh = form.children('#auto-refresh');
   var iframe = $('#theme-preview');
   var hash = window.location.hash;
-  
+  var appearanceSelector = $('#appearance-selector summary');
+
+  function fontSelector (selected) {
+    var select = $('<select></select>');
+    select.append('<option value="">(default)</option>');
+    select.append("<option value=\"Arial\" style=\"font-family:Arial, 'Helvetica Neue', Helvetica, sans-serif;\">Arial</option>");
+    select.append("<option value=\"Arial Black\" style=\"font-family:'Arial Black', Arial, 'Helvetica Neue', Helvetica, sans-serif;\">Arial Black</option>");
+    select.append("<option value=\"Baskerville\" style=\"font-family:Baskerville, 'Times New Roman', Times, serif;\">Baskerville</option>");
+    select.append("<option value=\"Century Gothic\" style=\"font-family:'Century Gothic', 'Apple Gothic', sans-serif;\">Century Gothic</option>");
+    select.append("<option value=\"Copperlate Light\" style=\"font-family:'Copperplate Light', 'Copperplate Gothic Light', serif;\">Copperlate Light</option>");
+    select.append("<option value=\"Courier New\" style=\"font-family:'Courier New', Courier, monospace;\">Courier New</option>");
+    select.append("<option value=\"Futura\" style=\"font-family:Futura, 'Century Gothic', AppleGothic, sans-serif;\">Futura</option>");
+    select.append("<option value=\"Garamond\" style=\"font-family:Garamond, 'Hoefler Text', Times New Roman, Times, serif;\">Garamond</option>");
+    select.append("<option value=\"Geneva\" style=\"font-family:Geneva, 'Lucida Sans', 'Lucida Grande', 'Lucida Sans Unicode', Verdana, sans-serif;\">Geneva</option>");
+    select.append("<option value=\"Georgia\" style=\"font-family:Georgia, Palatino, 'Palatino Linotype', Times, 'Times New Roman', serif;\">Georgia</option>");
+    select.append("<option value=\"Helvetica\" style=\"font-family:Helvetica, Arial, sans-serif;\">Helvetica</option>");
+    select.append("<option value=\"Helvetica Neue\" style=\"font-family:'Helvetica Neue', Arial, Helvetica, sans-serif;\">Helvetica Neue</option>");
+    select.append("<option value=\"Impact\" style=\"font-family:Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;\">Impact</option>");
+    select.append("<option value=\"Lucida Sans\" style=\"font-family:'Lucida Sans', 'Lucida Grande', 'Lucida Sans Unicode', sans-serif;\">Lucida Sans</option>");
+    select.append("<option value=\"Trebuchet MS\" style=\"font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;\">Trebuchet MS</option>");
+    select.append("<option value=\"Verdana\" style=\"font-family:Verdana, Geneva, Tahoma, sans-serif;\">Verdana</option>");
+    if (selected && select.children("option[value='"+selected+"']").length) {
+      select.children("option[value='"+selected+"']").attr('selected','selected');
+    }
+    return select;
+  }
+
+  function setAppearanceOptions (options) {
+    var selector = $('#appearance-selector .options');
+    selector.children(':not(:submit)').remove();
+    $.each(options, function (key, value) {
+      $.each(value, function (name, content) {
+        var p = $('<p><label>'+name+'</label></p>');
+        switch (key) {
+        case 'Boolean':
+          p.prepend('<input type="checkbox" name="'+name+'" '+(parseInt(content) ? 'checked' : '')+' />');
+          break;
+        case 'Font':
+          p.append(fontSelector(content).attr('name',name));
+          break;
+        default:
+          p.append('<input type="text" value="'+content+'" name="'+name+'" />');
+          break; 
+        }
+        p.insertBefore(selector.children(':submit'));
+      });
+    });
+  }
+
+  appearanceSelector.bind('click', function (e) {
+    var options = $(e.target).siblings('.options');
+    var parent = options.parent();
+    if (options.is(':hidden')) {
+      parent.addClass('open');
+      options.show();
+    } else {
+      parent.removeClass('open');
+      options.hide();
+    }
+  });
+
   form.bind('submit',function(e){
     var location;
     if (window.location.hash) {
@@ -58,5 +119,44 @@ $(document).ready(function(){
   } else {
     form.trigger('submit');
   }
+
+  iframe.bind('load', function (e) {
+    var frame = $(this.contentWindow? this.contentWindow.document : this.contentDocument.defaultView.document);
+    var metaElements = frame.find('meta[name]');
+    var appearanceOptions = { 
+      'Color': {},
+      'Font': {},
+      'Boolean': {},
+      'Text': {},
+      'Image': {}
+    };
+    metaElements.each( function (i, val) {
+      var $this = $(this);
+      var option = $this.attr('name').split(':');
+      var value = $this.attr('content');
+      if (option.length > 1) {
+        switch (option[0]) {
+        case 'if':
+          appearanceOptions['Boolean'][option[1]] = value;
+          break;
+        case 'color':
+          appearanceOptions['Color'][option[1]] = value;
+          break;
+        case 'font':
+          appearanceOptions['Font'][option[1]] = value;
+          break;
+        case 'image':
+          appearanceOptions['Image'][option[1]] = value;
+          break;
+        case 'text':
+          appearanceOptions['Text'][option[1]] = value;
+          break;
+        default:
+          throw "This is not a recognized meta apperance option.";
+        }
+      }
+    });
+    setAppearanceOptions(appearanceOptions);
+  });
   
 });
--- index.php
+++ index.php
@@ -13,6 +13,11 @@
 		#theme-select label { font-family: 'Bookman Old Style', Georgia, serif; font-weight: bold; font-size: 18px; vertical-align: middle; padding-right: 8px;}
 		#theme-select .small { font-family: Helvetica, Arial, sans-serif; font-weight: normal; font-size: 11px;}
 		#theme-select a { float: right; color: #fff; }
+    #theme-select #appearance-selector { position: relative; margin: 0 20px 0 5px; }
+    #theme-select #appearance-selector summary { cursor: pointer; padding: 0 5px; display: inline-block; }
+    #appearance-selector.open summary { background: #fff; color: #2c4762; }
+    #theme-select #appearance-selector .options { width: 300px; padding: 0 1em 1em; background: rgba(221, 221, 221, 0.9); border: 1px solid #ddd; overflow: auto; position: absolute; left: 0; z-index: 5; display: none; color: #444; line-height: 1.3;}
+    #theme-select .options label { font-family: Helvetica, Arial, sans-serif; font-weight: normal; font-size: 100%; padding: 0; margin-right: 5px; }
 		#theme-container {position:absolute; left:0px; right:0px; top:30px; bottom:0px;}
 		#theme-preview {width:100%; height:100%;}
 	</style>
@@ -23,14 +28,19 @@
 		<select name="theme" id="theme-selector">
 			<?php
 				foreach (glob('themes/*.html') as $theme) {
-                    $theme = basename($theme);
+          $theme = basename($theme);
 					if (($theme !== '.') && ($theme !== '..')) {
-						echo "<option value=\"$theme\">$theme</option>
-";
+						echo "<option value=\"$theme\">$theme</option>\n";
 					}
 				}
 			?>
 		</select>
+    <details id="appearance-selector">
+      <summary>Appearance</summary>
+      <div class="options">
+        <input type="submit" value="OK" />
+      </div>
+    </details>
 		<input type="checkbox" name="auto-refresh" id="auto-refresh">
 		<label for="auto-refresh" class="small">Auto Refresh?</label>
 		<a href="http://www.tumblr.com/docs/en/custom_themes">Theme Documentation</a>
