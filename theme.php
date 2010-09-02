3ba2aa1e54fe9e97628364ef24010d5c7222a6bd
Mark Wunsch <mark@markwunsch.com>
2010-09-01 22:09:58 -0400

    Passing meta options into the parser

--- theme.php
+++ theme.php
@@ -4,6 +4,9 @@ require_once 'parser/parser.php';
 
 $DATA = 'demo.yml';
 $THEME = 'themes/'.$_GET['theme'];
+$options = $_REQUEST;
+
+unset($options['theme'], $options['auto-refresh']);
 
 $last_modified_time = filemtime($THEME); 
 $etag = md5_file($THEME); 
@@ -12,12 +15,12 @@ header("Last-Modified: ".gmdate("D, d M Y H:i:s", $last_modified_time)." GMT");
 header("Etag: $etag");
 
 if (@strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) == $last_modified_time || 
-    trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) { 
-    header("HTTP/1.1 304 Not Modified"); 
-    exit; 
+   trim($_SERVER['HTTP_IF_NONE_MATCH']) == $etag) { 
+   header("HTTP/1.1 304 Not Modified"); 
+   exit; 
 }
 
 $theme = new ThimbleParser(file_get_contents('data/'.$DATA));
-echo $theme->parse(file_get_contents($THEME));
+echo $theme->parse(file_get_contents($THEME), $options);
 
 ?>
